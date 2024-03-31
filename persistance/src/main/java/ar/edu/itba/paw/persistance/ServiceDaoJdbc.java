d28412d778355d9a645b0c35cd9101193a2bce9b
UrielArias <urielarias.a12@gmail.com>
2024-03-31 15:29:19 -0300

    fix(persistence): defining naive implementations in ServiceDaoJdbc to new interface methods (not final)

--- persistance/src/main/java/ar/edu/itba/paw/persistance/ServiceDaoJdbc.java
+++ persistance/src/main/java/ar/edu/itba/paw/persistance/ServiceDaoJdbc.java
@@ -15,4 +15,22 @@ public class ServiceDaoJdbc implements ServiceDao {
         //TODO: Implementar
         return Optional.of(new Service(id, 1,"Nombre del servicio", "Breve descripcion del servicio prestado", Boolean.TRUE , "Callao 400", null, Categories.LIMPIEZA.getValue(), 60, PricingTypes.PER_TOTAL.getValue(), "$4000", Boolean.TRUE));
     }
+
+    @Override
+    public Service create(long businessid, String name, String description, Boolean homeservice, String location, Categories category, PricingTypes pricing, int price, Boolean additionalCharges){
+        //TODO: Implementar
+        return null;
+    }
+
+    @Override
+    public Service edit(long serviceid, String field, String newvalue) {
+        //TODO: Implementar
+        return null;
+    }
+
+    @Override
+    public Boolean delete(long serviceid) {
+        //TODO: Implementar
+        return null;
+    }
 }
