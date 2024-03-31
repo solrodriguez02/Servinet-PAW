4b90372f86ef2b7d31a0e2534d65435605192d55
UrielArias <urielarias.a12@gmail.com>
2024-03-31 15:25:36 -0300

    fix(persistence): returning optional after changes to return value's type in serviceDao

--- persistance/src/main/java/ar/edu/itba/paw/persistance/ServiceDaoJdbc.java
+++ persistance/src/main/java/ar/edu/itba/paw/persistance/ServiceDaoJdbc.java
@@ -1,14 +1,18 @@
 package ar.edu.itba.paw.persistance;
 
+import ar.edu.itba.paw.model.Categories;
+import ar.edu.itba.paw.model.PricingTypes;
 import ar.edu.itba.paw.model.Service;
 import ar.edu.itba.paw.services.ServiceDao;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class ServiceDaoJdbc implements ServiceDao {
     @Override
-    public Service findById(long id) {
-
-        return new Service(id, "Nombre del servicio", "Breve descripcion del servicio prestado", "Ubicacion", "Categoria");
+    public Optional<Service> findById(long id) {
+        //TODO: Implementar
+        return Optional.of(new Service(id, 1,"Nombre del servicio", "Breve descripcion del servicio prestado", Boolean.TRUE , "Callao 400", null, Categories.LIMPIEZA.getValue(), 60, PricingTypes.PER_TOTAL.getValue(), "$4000", Boolean.TRUE));
     }
 }
