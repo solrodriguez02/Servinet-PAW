5bdcd9a7fd75852681f4404edf06495615c5d390
UrielArias <urielarias.a12@gmail.com>
2024-04-01 12:13:31 -0300

    style(interfaces): missing parameter in create method

--- interfaces/src/main/java/ar/edu/itba/paw/services/ServiceService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/services/ServiceService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface ServiceService {
     Optional<Service> findById(long id);
-    Service create(long businessid, String name, String description, Boolean homeservice, String location, Categories category, PricingTypes pricing, int price, Boolean additionalCharges);
+    Service create(long businessid, String name, String description,Boolean homeservice, String location, Categories category, int minimalduration, PricingTypes pricing, String price, Boolean additionalCharges);
     Service edit(long serviceid, String field, String newvalue);
     Boolean delete(long serviceid);
 
