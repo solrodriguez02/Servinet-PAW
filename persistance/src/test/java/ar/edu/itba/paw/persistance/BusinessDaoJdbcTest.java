c3f72c98fe8167198b930f667e57fae4b38ad17d
nicol <ncasella@itba.edu.ar>
2024-04-01 01:56:05 -0300

    test: added BusinessDao/service tests

---
+++ persistance/src/test/java/ar/edu/itba/paw/persistance/BusinessDaoJdbcTest.java
@@ -0,0 +1,57 @@
+package ar.edu.itba.paw.persistance;
+
+
+import ar.edu.itba.paw.model.Business;
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.persistance.config.TestConfig;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import javax.sql.DataSource;
+
+@Sql("classpath:sql/schema.sql")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+public class BusinessDaoJdbcTest {
+
+    private static final long ID =1;
+    private static final String BUSINESS_NAME = "Business";
+    private static final long USER_ID = 1;
+    private static final String TELEPHONE = "123456789";
+    private static final String EMAIL = "mail@mail.com";
+    private static final String LOCATION = null;
+
+    @Autowired
+    private BusinessDaoJdbc businessDaoJdbc;
+    @Autowired
+    private DataSource ds;
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setup() {
+        this.jdbcTemplate = new JdbcTemplate(ds);
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+    }
+
+    @Test
+    public void testCreateBusiness() {
+        // 1. Precondiciones
+        jdbcTemplate.execute("INSERT INTO users (userid, username, password, name, surname, email, telephone) VALUES (1, 'username', 'password', 'name', 'surname', 'email', 'telephone')");
+
+        // 2. Ejecuta la class under test (una sola)
+        Business business= businessDaoJdbc.createBusiness(BUSINESS_NAME, 1, TELEPHONE, EMAIL, LOCATION);
+
+        // 3. Postcondiciones - assertions (todas las que sean necesarias)
+        Assert.assertNotNull(business);
+        Assert.assertEquals(BUSINESS_NAME, business.getBusinessName());
+        Assert.assertEquals(1,JdbcTestUtils.countRowsInTable(jdbcTemplate, "business"));
+    }
+
+}
