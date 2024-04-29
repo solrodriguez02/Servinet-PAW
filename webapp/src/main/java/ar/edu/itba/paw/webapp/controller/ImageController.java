8cec64e10121aa74778f4c216c373de1842c698d
nicol <ncasella@itba.edu.ar>
2024-04-29 17:10:09 -0300

    fix: moved image implementation to webapp module

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ImageController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ImageController.java
@@ -6,18 +6,25 @@ import ar.edu.itba.paw.services.ImageService;
 import ar.edu.itba.paw.webapp.exception.ImageNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.util.Optional;
+
 
 @Controller
 public class ImageController {
     private final ImageService is;
-
+    @Value("resources/defaultImg.png")
+    private Resource defaultImage;
     @Autowired
     public ImageController(@Qualifier("imageServiceImpl") ImageService is) {
         this.is = is;
@@ -25,7 +32,14 @@ public class ImageController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/images/{imageId:\\d+}", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] images(@PathVariable("imageId") final int imageId) {
-        ImageModel img = is.getImageById(imageId).orElseThrow(ImageNotFoundException::new);
-        return img.getImageBytes();
+        final Optional<ImageModel> image = is.getImageById(imageId);
+        if(image.isEmpty()){
+            try{
+                return StreamUtils.copyToByteArray(defaultImage.getInputStream());
+            } catch (IOException e) {
+                throw new ImageNotFoundException();//TODO:imprimir en log
+            }
+        }
+        return image.get().getImageBytes();
     }
 }
