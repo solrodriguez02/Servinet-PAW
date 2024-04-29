240e46307b69fbad1ab9493c89ed35a8f6c99ade
nicol <ncasella@itba.edu.ar>
2024-04-29 15:28:27 -0300

    test: updated tests

--- persistance/src/test/resources/sql/schema.sql
+++ persistance/src/test/resources/sql/schema.sql
@@ -1,76 +1,29 @@
---CONSULTAR: como compartir archivos schema.sql en test y main, problemas de incompatibilidad de sintaxis
-
-/*
-
---------------- VERSION ANTERIOR, FIJARSE DE CAMBIAR LOS TESTS EN BASE AL NUEVO SCHEMA -------------------
-
-
-DROP TABLE IF EXISTS appointments;
-DROP TABLE IF EXISTS services;
-DROP TABLE IF EXISTS business;
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE IF NOT EXISTS users (
-                                     userid SERIAL PRIMARY KEY,
-                                     username VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL, --todo: sacar el default null una vez que se implemente el login
-                                     name VARCHAR(255) NOT NULL,
-                                     surname VARCHAR(255) NOT NULL,
-                                     email VARCHAR(255) NOT NULL UNIQUE,
-                                     telephone VARCHAR(255),
-                                     isprovider boolean NOT NULL DEFAULT FALSE
-);
-
-CREATE TABLE IF NOT EXISTS business(
-                                       businessid SERIAL PRIMARY KEY,
-                                       userid INT references users(userid) ON DELETE CASCADE,
-                                       businessname VARCHAR(255),
-                                       businessTelephone VARCHAR(255),
-                                       businessEmail VARCHAR(255),
-                                       businessLocation VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS services (
-                                        id SERIAL PRIMARY KEY,
-                                        businessid INT REFERENCES business(businessid) ON DELETE CASCADE,
-    servicename VARCHAR(255) NOT NULL,
-    servicedescription VARCHAR(255),
-    homeservice BOOLEAN,
-    location VARCHAR(255) NOT NULL,
-    category  VARCHAR(50) CHECK (category IN ('Limpieza', 'Belleza', 'Arreglos calificados', 'Mascotas', 'Exteriores', 'Eventos y Celebraciones', 'Transporte', 'Consultoria', 'Salud')),
-    minimalduration INT,
-    pricingtype  VARCHAR(50) CHECK (pricingtype IN ('Per Hour', 'Per Total', 'Budget', 'TBD')),
-    price VARCHAR(255),
-    additionalcharges BOOLEAN
-    );
-
-*/
-
-
-CREATE TABLE IF NOT EXISTS users (
-     userid SERIAL PRIMARY KEY,
-     username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    userid SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, --todo: sacar el default null una vez que se implemente el login
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     telephone VARCHAR(255),
     isprovider boolean NOT NULL DEFAULT FALSE
-    );
+);
 
 CREATE TABLE IF NOT EXISTS business(
-   businessid SERIAL PRIMARY KEY,
-   userid INT references users(userid) ON DELETE CASCADE,
+    businessid SERIAL PRIMARY KEY,
+    userid INT references users(userid) ON DELETE CASCADE,
     businessname VARCHAR(255),
     businessTelephone VARCHAR(255),
     businessEmail VARCHAR(255),
     businessLocation VARCHAR(255)
-    );
-
-CREATE TABLE IF NOT EXISTS images(
-     imageid SERIAL PRIMARY KEY,
-     imageBytes BYTEA[]
 );
+CREATE TABLE IF NOT EXISTS images(
+    imageid SERIAL PRIMARY KEY,
+    imageBytes binary
+);
+
+--CREATE TYPE serviceCategory AS ENUM ('Limpieza', 'Belleza', 'Arreglos calificados', 'Mascotas', 'Exteriores', 'Eventos y Celebraciones', 'Transporte', 'Consultoria', 'Salud');
+--CREATE TYPE pricingType AS ENUM ('Per hour', 'Per total', 'Budget', 'TBD');
 
 CREATE TABLE IF NOT EXISTS services (
     id SERIAL PRIMARY KEY,
@@ -85,8 +38,21 @@ CREATE TABLE IF NOT EXISTS services (
     price VARCHAR(255),
     additionalcharges BOOLEAN,
     imageId INT references images(imageid),
-    neighbourhood VARCHAR(255)
-    );
+);
+
+CREATE TABLE IF NOT EXISTS nbservices(insertid serial primary key ,
+    serviceid int references services(id) on delete cascade ,
+    neighbourhood varchar(60)
+);
+
+--script para generar la nueva tabla
+--begin transaction;
+--alter table services add neighbourhood varchar(255);
+--update services set neighbourhood = split_part(location,';',1);
+--update services set location = split_part(location,';',2);
+--create table nbservices(insertid serial primary key, serviceid int references services(id) on delete cascade, neighbourhood text);
+--insert into nbservices(serviceid,neighbourhood) select id,neighbourhood from services;
+--commit;
 
 CREATE TABLE IF NOT EXISTS appointments (
     appointmentid SERIAL PRIMARY KEY,
@@ -95,8 +61,8 @@ CREATE TABLE IF NOT EXISTS appointments (
     startDate TIMESTAMP NOT NULL,
     endDate TIMESTAMP,
     location VARCHAR(255),
-    confirmed BOOLEAN DEFAULT FALSE
-    );
+    confirmed BOOLEAN NOT NULL DEFAULT FALSE
+);
 
 CREATE TABLE IF NOT EXISTS questions (
     questionid SERIAL PRIMARY KEY,
@@ -105,7 +71,7 @@ CREATE TABLE IF NOT EXISTS questions (
     question VARCHAR(255) NOT NULL,
     response VARCHAR(255),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+);
 
 CREATE TABLE IF NOT EXISTS ratings (
     ratingid SERIAL PRIMARY KEY,
@@ -114,4 +80,23 @@ CREATE TABLE IF NOT EXISTS ratings (
     rating INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
     comment VARCHAR(255),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+);
+-- insert into users(username, name, surname, email, telephone, password, isprovider) values ('solro', 'sol', 'rodri', 'solrodriguezgiana@gmail.com', '113452343', 'solro', true);
+-- insert into business (userid, businessname, businessTelephone, businessEmail, businessLocation) values (1, 'Sol nails shop', '11365335', 'solrodriguezgiana@gmail.com', 'Palermo');
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas francecita', 'Servicio de uñas francesitas.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '5000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Limpieza de cuticula', 'Servicio de uñas: limpieza de cuticula express.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '3000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Ya no se que otro servicio inventar de uñas', 'Servicio de uñas: necesito que este texto sea largo. Chatgpt: La vida es un viaje lleno de sorpresas y aventuras. Cada día es una oportunidad para explorar, aprender y crecer. .', TRUE, 'Palermo', 'Belleza', 60, 'Por hora', '3000', FALSE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping1', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping2', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Limpieza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping3', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping4', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping5', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping6', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping7', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping8', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Belleza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping9', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Limpieza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping10', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Limpieza', 60, 'Por hora', '10000', TRUE);
+-- insert into services(businessid, servicename, servicedescription, homeservice, location, category, minimalduration, pricingtype, price, additionalcharges) values (1, 'Uñas capping11', 'Servicio de uñas, multiples colores y esmaltes de todo tipo. Diseño a eleccion del cliente. Arte en uñas. Consulte por disponibilidad.', FALSE, 'Palermo', 'Limpieza', 60, 'Por hora', '10000', TRUE);
+
+
