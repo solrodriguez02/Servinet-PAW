bdc9c258b81f1d547b218f2ff0a6b9902b15b614
UrielArias <urielarias.a12@gmail.com>
2024-05-12 14:58:00 -0300

    fix(controller): deleting unused method and moving get method to the same controller as post

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AppointmentController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AppointmentController.java
@@ -35,9 +35,26 @@ public class AppointmentController {
         this.authControl= authControl;
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/contratar-servicio/{serviceId:\\d+}")
+    public ModelAndView hireService(@PathVariable("serviceId") final long serviceId, @ModelAttribute("appointmentForm") final AppointmentForm form) {
+
+        final ModelAndView mav = new ModelAndView("postAppointment");
+        try {
+            Service service = serviceService.findById(serviceId).orElseThrow(ServiceNotFoundException::new);
+            mav.addObject("service",service);
+        } catch (ServiceNotFoundException ex) {
+            return new ModelAndView("redirect:/operacion-invalida/?argumento=servicionoexiste");
+        }
+
+        return mav;
+    }
+
     @RequestMapping(method = RequestMethod.POST, path = "/contratar-servicio/{serviceId:\\d+}")
     public ModelAndView appointment(@PathVariable("serviceId") final long serviceId, @Valid @ModelAttribute("appointmentForm") AppointmentForm form, BindingResult errors) {
         //todo: manejo de errores de ingreso del formulario (se lanzarían excepciones a nivel sql)
+        if(errors.hasErrors()) {
+            return hireService(serviceId, form);
+        }
         User user = authControl.getCurrentUser().get();
 
         Appointment createdAppointment = appointmentService.create(serviceId,user.getName(),user.getSurname(),user.getEmail(),form.getLocation(),user.getEmail(), form.getDate().toString());
--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -112,15 +112,6 @@ public class HelloWorldController {
     //     return mav;
     // }
 
-    @RequestMapping(method = RequestMethod.GET, path="/publicar")
-    public ModelAndView publish() {
-        final ModelAndView mav = new ModelAndView("publish");
-        User currentUser = authControl.getCurrentUser().get();
-        if(!currentUser.isProvider()) {
-            return new ModelAndView("redirect:/registrar-negocio");
-        }
-        return mav;
-    }
 //@RequestMapping(method = RequestMethod.GET, path="/publicar")
 //    public ModelAndView publish() {
 //@RequestParam(value = "nombre-negocio") final String businessName,
