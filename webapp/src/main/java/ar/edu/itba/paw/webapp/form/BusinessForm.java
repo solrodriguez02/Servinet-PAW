ba504c33ebba762a0bf17fff11608dbc7448a9fd
UrielArias <urielarias.a12@gmail.com>
2024-05-12 12:05:34 -0300

    feat(webapp): adding validations to model attributes AppointmentForm and BusinessForm

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/BusinessForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/BusinessForm.java
@@ -1,9 +1,20 @@
 package ar.edu.itba.paw.webapp.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class BusinessForm {
+    @NotEmpty
+    @Size(max=255, message="El nombre del negocio no puede superar los 255 caracteres")
     private String businessName;
+    @NotEmpty
     private String businessEmail;
+    @NotEmpty
+    //regex? pueden haber varios formatos (se podría ver con front)
     private String businessTelephone;
+    @NotEmpty
+    @Size(max=255, message="La ubicación del negocio no puede superar los 255 caracteres")
+    //regex? calle, altura, barrio(CABA), depto (?), piso (?) -> (?) opcionales
     private String businessLocation;
 
     public String getBusinessName() {
