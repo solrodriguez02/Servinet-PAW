f71420fad9a31327fc97f88349e5b151d4b3e958
nicol <ncasella@itba.edu.ar>
2024-05-13 10:00:19 -0300

    fix:fixed date validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validation/FutureDateValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validation/FutureDateValidator.java
@@ -1,13 +1,23 @@
 package ar.edu.itba.paw.webapp.validation;
 
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
 public class FutureDateValidator implements ConstraintValidator<FutureDate,String> {
 
+
     @Override
     public boolean isValid(String dateTime, ConstraintValidatorContext context) {
-        return dateTime !=null && LocalDateTime.parse(dateTime).isAfter(LocalDateTime.now());
+        if (dateTime ==null || dateTime.isEmpty())
+            return false;
+        try {
+            return LocalDateTime.parse(dateTime).isAfter(LocalDateTime.now());
+        }
+        catch (DateTimeParseException e) {
+            return false;
+        }
     }
 }
