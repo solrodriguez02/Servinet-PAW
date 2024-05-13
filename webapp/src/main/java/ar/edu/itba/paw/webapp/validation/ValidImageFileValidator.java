7c1e4800e3d792837b5fd9a363d93fb643371b5e
nicol <ncasella@itba.edu.ar>
2024-05-13 00:47:11 -0300

    fix:added missing file size validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validation/ValidImageFileValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validation/ValidImageFileValidator.java
@@ -2,14 +2,14 @@ package ar.edu.itba.paw.webapp.validation;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Constraint;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ValidImageFileValidator implements ConstraintValidator<ValidImageFile, MultipartFile> {
    @Override
    public boolean isValid( MultipartFile file, ConstraintValidatorContext context) {
-       return file == null || file.isEmpty() || (file.getContentType().contains("image/jpg") || file.getContentType().contains("image/jpeg") || file.getContentType().contains("image/png"));
+       return file == null || file.isEmpty() || ( file.getContentType()!=null && file.getSize() < 2*1024*1024 && (file.getContentType().contains("image/jpg") || file.getContentType().contains("image/jpeg") || file.getContentType().contains("image/png")));
    }
 
 }
